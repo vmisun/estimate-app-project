cdde0c2a5625b358c294e649cf847f89b997096d
vmisun <karolina.gniadzik@icloud.com>
2022-04-10 21:11:07 +0200

    Deployed website

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
           <a
             href="mailto:karolina.gniadzik@icloud.com"
             class="col-sm-6 mt-3"
-            title="Napisz e-mail Karolina Gniadzik"
+            title="Napisz e-mail do autora"
           >
             <i class="fa-solid fa-at"></i>
           </a>
